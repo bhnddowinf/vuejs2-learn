b088c6343399dbdc79f5ffb0b5b6427844f49854
bhnddowinf <bhnddowinf@yahoo.com.tw>
2016-10-16 21:56:02 +0800

    update

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -3,24 +3,11 @@
     <img class="logo" src="./assets/logo.png">
     <hello></hello>
     <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
+      欢迎大家来学习 台湾小凡最爱的 Vue.js app!
     </p>
 
     <div align="center">
+    <a href='https://github.com/bhnddowinf/vuejs2-learn'>台湾小凡 vuejs2 官网讲解</a>>
     <ul>
       <li><a href='h0201.html'>h0201.html</a></li>
     </ul>
